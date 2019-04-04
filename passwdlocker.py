962acc10d9fd04b633ce2c8a105118ef2f2d3819
Neeraj Panwar <49247372+neeraj3030@users.noreply.github.com>
2019-04-04 21:54:44 +0530

    Update passwdlocker.py

--- passwdlocker.py
+++ passwdlocker.py
@@ -8,15 +8,15 @@ import sys
 import pyperclip
 
 PASSWORDS = {'email':'user12324@gmail.com',
-			'usr':'admin',
+		'usr':'admin',
              'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',	
-			'github':'something',
+		'github':'something',
              'admin': '1234045'}
 
 #sys.argv is a list containing commandline arguments,including the name of the file as 0th item in list 
 
 if len(sys.argv) < 2:
-    print('Usage: python pw.py [account] - copy account password')
+    print('Usage: python passwdlocker.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1]      # first command line arg is the account name 
